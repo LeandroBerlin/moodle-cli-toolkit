ee0ab7a9de7e441f091e51963862e820ff81b925
leandro frigerio <leandro.frigerio@turbinekreuzberg.com>
2022-08-22 09:56:17 +0200

    add database backups

--- cli.sh
+++ cli.sh
@@ -6,10 +6,7 @@ MOODLE_PLATFORM_SHORTNAME="Moodle Learning"
 MOODLE_PLATFORM_SUMMARY="Learning platform"
 MOODLE_PLATFORM_ADMIN_PASS="change123"
 MOODLE_PLATFORM_ADMIN_EMAIL="admin@example.com"
-MOODLE_DOCKER_WWWROOT=$( pwd; )/moodle/
-MOODLE_DOCKER_PATH=$( pwd; )/moodle-docker/
-MOODLE_DOCKER_DB=pgsql
-MOODLE_DOCKER_PHP_VERSION=7.3
+DATABASE_BACKUP_DIR="db_backups"
 #set this if using a remote docker host
 #MOODLE_DOCKER_WEB_HOST=192.168.0.60
 
@@ -25,6 +22,7 @@ help(){
    echo "--stop     Stop the development environment"
    echo "--delete   Stop and delete the docker containers"
    echo "--reset    Cleanup, remove docker and Moodle dirs"
+   echo "--dump     Create a dump of the database"
    echo "--help     Print this help."
    echo
 }
@@ -86,6 +84,15 @@ reset(){
   rm -rf $MOODLE_DOCKER_PATH && rm -rf $MOODLE_DOCKER_WWWROOT
 }
 
+dump(){
+  echo "[+] Dump database"
+  if [[ ! -e $DATABASE_BACKUP_DIR ]]; then
+      mkdir $DATABASE_BACKUP_DIR
+  fi
+  docker exec -t moodle-docker-db-1 pg_dumpall -c -U moodle > $DATABASE_BACKUP_DIR/dump_`date +%d-%m-%Y"_"%H_%M_%S`.sql
+  echo "[+] Dump database created in $DATABASE_BACKUP_DIR"
+}
+
 
 if [[ $# -eq 0 ]] ; then
     help
@@ -97,6 +104,7 @@ else
   	--stop) stop ;;
   	--delete) delete ;;
   	--reset) reset ;;
+    --dump) dump ;;
   	--help) help ;;
 
   	*) echo -e "[-] Command $1 not recognized\n" && help ;;
